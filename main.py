445d6697cf39e0eec1f5ef278f30de585e756de5
Karthik Karanth <karanth.karthik@gmail.com>
2020-04-29 12:48:19 -0600

    try click for VOI extraction

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def load_volume(filename):
     (xSpacing, ySpacing, zSpacing) = reader.GetOutput().GetSpacing()
     (x0, y0, z0) = reader.GetOutput().GetOrigin()
     print(f'Origin: {(x0, y0, z0)}')
+    print(f'Extents: min: {(xMin, yMin, zMin)}')
+    print(f'         max: {(xMax, yMax, zMax)}')
     center = [x0 + xSpacing * 0.5 * (xMin + xMax),
             y0 + ySpacing * 0.5 * (yMin + yMax),
             z0 + zSpacing * 0.5 * (zMin + zMax)]
@@ -48,6 +50,41 @@ def load_volume(filename):
 
     return volume, reader.GetOutputPort()
 
+def volume_subset(image):
+    voi = vtk.vtkExtractVOI()
+    voi.SetInputConnection(image)
+    voi.SetVOI(0, 447, 0, 447, 0, 127)
+
+    mapper = vtk.vtkFixedPointVolumeRayCastMapper()
+    mapper.SetInputConnection(voi.GetOutputPort())
+    # The following class is used to store transparency-values for later retrival.
+    #  In our case, we want the value 0 to be
+    # completely opaque whereas the three different cubes are given different transparency-values to show how it works.
+    alphaChannelFunc = vtk.vtkPiecewiseFunction()
+    alphaChannelFunc.AddPoint(0, 0.0)
+    alphaChannelFunc.AddPoint(50, 0.05)
+    alphaChannelFunc.AddPoint(100, 0.1)
+    alphaChannelFunc.AddPoint(150, 0.2)
+
+    # This class stores color data and can create color tables from a few color points.
+    #  For this demo, we want the three cubes to be of the colors red green and blue.
+    colorFunc = vtk.vtkColorTransferFunction()
+    colorFunc.AddRGBPoint(50, 1.0, 0.0, 0.0)
+    colorFunc.AddRGBPoint(100, 0.0, 1.0, 0.0)
+    colorFunc.AddRGBPoint(150, 0.0, 0.0, 1.0)
+
+    # The previous two classes stored properties.
+    #  Because we want to apply these properties to the volume we want to render,
+    # we have to store them in a class that stores volume properties.
+    volumeProperty = vtk.vtkVolumeProperty()
+    volumeProperty.SetColor(colorFunc)
+    volumeProperty.SetScalarOpacity(alphaChannelFunc)
+
+    volume = vtk.vtkVolume()
+    volume.SetMapper(mapper)
+    volume.SetProperty(volumeProperty)
+    return voi, volume
+
 def load_slice(image):
     im = vtk.vtkImageResliceMapper()
     im.SetInputConnection(image)
@@ -72,30 +109,57 @@ def load_slice(image):
 
     return im, ia
 
+debug_sphere = None
 # To handle click events
 class MouseInteractorHighLightActor(vtk.vtkInteractorStyleTrackballCamera):
  
     def __init__(self,parent=None):
         self.AddObserver("LeftButtonPressEvent",self.leftButtonPressEvent)
+        self.subset = None
+    
+    def set_voi(self, voi):
+        self.voi = voi
  
     def leftButtonPressEvent(self,obj,event):
+        global debug_sphere
         self.OnLeftButtonDown()
         clickPos = self.GetInteractor().GetEventPosition()
 
         picker = vtk.vtkCellPicker()
         picker.Pick(clickPos[0], clickPos[1], 0, self.GetDefaultRenderer())
-        world_pos = picker.GetPickPosition()
+        world_pos = [int(x) for x in picker.GetPickPosition()]
+        maxes = [447, 447, 127]
+        if any((world_pos[i] < 0 or world_pos[i] > maxes[i]) for i in range(3)):
+            self.OnLeftButtonDown()
+            return
+
         print(world_pos)
+        world_pos = (114, 114, 50)
+        debug_sphere.SetCenter(*world_pos)
+
+        d = 35 # Size of box
+        voi_min = [max(x - d, 0) for x in world_pos]
+        voi_max = [min(world_pos[i] + d, maxes[i]) for i in range(3)]
+        print(voi_min, voi_max)
+        self.voi.SetVOI(
+            voi_min[0], voi_max[0],
+            voi_min[1], voi_max[1],
+            voi_min[2], voi_max[2],
+        )
         
-        self.OnLeftButtonDown()
+        # self.OnLeftButtonDown()
 
 
 def main():
+    global debug_sphere
     # volume is the VTK object we render. image is loaded from the file
-    volume, image = load_volume('data/Normal-001/MRA/Normal001-MRA.mha')
+    _, image = load_volume('data/Normal-001/MRA/Normal001-MRA.mha')
 
     # load the slice
     slice_im, slice_ia = load_slice(image)
+
+    # Subset
+    voi, subset_actor = volume_subset(image)
 
     # Create the renderer
     renderer = vtk.vtkRenderer()
@@ -105,6 +169,7 @@ def main():
     renderInteractor.SetRenderWindow(renderWin)
     style = MouseInteractorHighLightActor()
     style.SetDefaultRenderer(renderer)
+    style.set_voi(voi)
     renderInteractor.SetInteractorStyle(style)
     # renderInteractor.GetInteractorStyle().SetCurrentStyleToTrackballCamera()
     colors = vtk.vtkNamedColors()
@@ -117,46 +182,59 @@ def main():
     camera.SetViewUp(0, 0.0, 1.0)
     renderer.SetActiveCamera(camera)
 
-    # We add the volume to the renderer
-    # renderer.AddVolume(volume)
 
-    # Add the slice
+    # Add stuff
     renderer.AddActor(slice_ia)
+    renderer.AddActor(subset_actor)
+    # renderer.AddVolume(volume)
 
     # ... and set window size.
     renderWin.SetSize(900, 900)
+
+    ##### DEBUG SPHERE
+    debug_sphere = vtk.vtkSphereSource()
+    debug_sphere.SetCenter(0,0,0)
+    debug_sphere.SetRadius(5.0)
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(debug_sphere.GetOutputPort())
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+
+    # assign actor to the renderer
+    renderer.AddActor(actor)
+    ############
 
     def vtkSliderCallback2(obj, event):
         new_slice = int(obj.GetRepresentation().GetValue())
         plane = slice_im.GetSlicePlane()
         plane.SetOrigin(new_slice, 114, 50)
 
-    SliderRepres = vtk.vtkSliderRepresentation2D()
-    SliderRepres.SetMinimumValue(0)
-    SliderRepres.SetMaximumValue(240)
-    SliderRepres.SetValue(144)
-    SliderRepres.SetTitleText("Slice")
-    SliderRepres.GetPoint1Coordinate().SetCoordinateSystemToNormalizedDisplay()
-    SliderRepres.GetPoint1Coordinate().SetValue(0.7, 0.9)
-    SliderRepres.GetPoint2Coordinate().SetCoordinateSystemToNormalizedDisplay()
-    SliderRepres.GetPoint2Coordinate().SetValue(0.9, 0.9)
+    slider = vtk.vtkSliderRepresentation2D()
+    slider.SetMinimumValue(0)
+    slider.SetMaximumValue(240)
+    slider.SetValue(144)
+    slider.SetTitleText("Slice")
+    slider.GetPoint1Coordinate().SetCoordinateSystemToNormalizedDisplay()
+    slider.GetPoint1Coordinate().SetValue(0.7, 0.9)
+    slider.GetPoint2Coordinate().SetCoordinateSystemToNormalizedDisplay()
+    slider.GetPoint2Coordinate().SetValue(0.9, 0.9)
 
-    SliderRepres.SetSliderLength(0.02)
-    SliderRepres.SetSliderWidth(0.03)
-    SliderRepres.SetEndCapLength(0.01)
-    SliderRepres.SetEndCapWidth(0.03)
-    SliderRepres.SetTubeWidth(0.005)
-    SliderRepres.SetLabelFormat("%3.0lf")
-    SliderRepres.SetTitleHeight(0.02)
-    SliderRepres.SetLabelHeight(0.02)
+    slider.SetSliderLength(0.02)
+    slider.SetSliderWidth(0.03)
+    slider.SetEndCapLength(0.01)
+    slider.SetEndCapWidth(0.03)
+    slider.SetTubeWidth(0.005)
+    slider.SetLabelFormat("%3.0lf")
+    slider.SetTitleHeight(0.02)
+    slider.SetLabelHeight(0.02)
 
-    SliderWidget = vtk.vtkSliderWidget()
-    SliderWidget.SetInteractor(renderInteractor)
-    SliderWidget.SetRepresentation(SliderRepres)
-    SliderWidget.KeyPressActivationOff()
-    SliderWidget.SetAnimationModeToAnimate()
-    SliderWidget.SetEnabled(True)
-    SliderWidget.AddObserver("InteractionEvent", vtkSliderCallback2)
+    slider_widget = vtk.vtkSliderWidget()
+    slider_widget.SetInteractor(renderInteractor)
+    slider_widget.SetRepresentation(slider)
+    slider_widget.KeyPressActivationOff()
+    slider_widget.SetAnimationModeToAnimate()
+    slider_widget.SetEnabled(True)
+    slider_widget.AddObserver("InteractionEvent", vtkSliderCallback2)
 
     # A simple function to be called when the user decides to quit the application.
     def exitCheck(obj, event):
